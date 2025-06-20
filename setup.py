1f02cb2f76f14b30e0a345641ae972500c53069c
Sergii Kalinchuk <sergii.kalinchuk@raccoongang.com>
2025-06-20 12:20:49 +0300

    Fix Python version

--- setup.py
+++ setup.py
@@ -143,7 +143,6 @@ setup(
 
     include_package_data=True,
     install_requires=load_requirements('requirements/base.in'),
-    python_requires=">=3.12",
     zip_safe=False,
     keywords='Python edx',
     classifiers=[
